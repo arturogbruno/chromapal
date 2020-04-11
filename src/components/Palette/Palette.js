35661700f2e41ae78a9dac3526aa43421ebc9e0d
Arturo G. Bruno <arturogbruno@gmail.com>
2020-04-11 21:18:52 +0200

    add styles to slider

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -26,7 +26,9 @@ class Palette extends Component {
         return(
             <div className="Palette">
                 {/* Navbar goes here */}
-                <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={this.changeLevel} />
+                <div className="slider">
+                    <Slider defaultValue={level} min={100} max={900} step={100} onChange={this.changeLevel} />
+                </div>
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
