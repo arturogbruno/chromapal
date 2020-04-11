946ff25eeeef8e4e9cf729c40e19102b0a57b821
Arturo G. Bruno <arturogbruno@gmail.com>
2020-04-11 20:04:47 +0200

    add color slider

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import seedPalettes from './seeds/seedPalettes';
 import { generatePalette } from './colorHelpers';
 
 function App() {
-    console.log(generatePalette(seedPalettes[4]));
     return (
         <div className="App">
-            <Palette {...seedPalettes[4]} />
+            <Palette palette={generatePalette(seedPalettes[4])} />
         </div>
     );
 }
