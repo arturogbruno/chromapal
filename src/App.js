1201efd2f41344fe58327c463a72b7a45f9ba9b0
Arturo G. Bruno <arturogbruno@gmail.com>
2020-04-12 18:16:39 +0200

    add Palette route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Palette from './components/Palette/Palette';
 import seedPalettes from './seeds/seedPalettes';
+import { generatePalette } from './colorHelpers';
 
-function App() {
-    return (
-        <Switch>
-            <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
-            <Route exact path="/palette/:id" render={() => <h1>Individual palette</h1>} />
-        </Switch>
-    );
+class App extends Component {
+    findPalette(id) {
+        return seedPalettes.find(palette => palette.id === id);
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+                <Route exact path="/palette/:id" render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} />
+            </Switch>
+        );
+    }
 }
 
 export default App;
