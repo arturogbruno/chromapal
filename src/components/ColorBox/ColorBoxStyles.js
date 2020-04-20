460e9b726e2d7a3195785540e15ee9ef890f91c1
Arturo G. Bruno <arturogbruno@gmail.com>
2020-04-20 20:05:18 +0200

    make ColorBox and SingleColorPalette responsive

--- src/components/ColorBox/ColorBoxStyles.js
+++ src/components/ColorBox/ColorBoxStyles.js
@@ -16,11 +16,11 @@ export default {
         },
         [sizes.down("lg")]: {
             width: "25%",
-            height: props => props.showingFullPalette ? "20%" : "50%",
+            height: props => props.showingFullPalette ? "20%" : "33.3333%",
         },
         [sizes.down("md")]: {
             width: "50%",
-            height: props => props.showingFullPalette ? "10%" : "50%",
+            height: props => props.showingFullPalette ? "10%" : "20%",
         },
         [sizes.down("xs")]: {
             width: "100%",
@@ -110,7 +110,10 @@ export default {
             textAlign: "center",
             backgroundColor: "rgba(255, 255, 255, 0.2)",
             marginBottom: "0",
-            padding: "1rem"
+            padding: "1rem",
+            [sizes.down("xs")]: {
+                fontSize: "6rem"
+            }
         },
         "& p": {
             fontSize: "2rem",
