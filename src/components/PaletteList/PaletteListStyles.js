cf1cb9bcba3bd8d75bf9a3a3a028e1e3e2ec3073
Arturo G. Bruno <arturogbruno@gmail.com>
2020-04-21 18:02:52 +0200

    make PaletteList responsive

--- src/components/PaletteList/PaletteListStyles.js
+++ src/components/PaletteList/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from '../../sizes';
+
 export default {
     root: {
         backgroundColor: "darkblue",
@@ -13,6 +15,12 @@ export default {
         flexDirection: "column",
         alignItems: "flex-start",
         flexWrap: "wrap",
+        [sizes.down("xl")]: {
+            width: "80%"
+        },
+        [sizes.down("xs")]: {
+            width: "75%"
+        }
     },
     header: {
         width: "100%",
@@ -30,6 +38,13 @@ export default {
         width: "100%",
         display: "grid",
         gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "5%"
+        gridGap: "2.5rem",
+        [sizes.down("md")]: {
+            gridTemplateColumns: "repeat(2, 50%)"
+        },
+        [sizes.down("xs")]: {
+            gridTemplateColumns: "repeat(1, 100%)",
+            gridGap: "1rem"
+        }
     }
 }
